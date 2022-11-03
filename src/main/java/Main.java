2174ddff53a032d35b674fbf79da1078f77aa0ff
ungabunga1 <yevgenfenoga@gmail.com>
2022-11-02 20:46:03 -0400

    print test2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cb29ed7b5ba7bad3ab67c570ad2fae623935e53f2f0c16896981e18e4c5bb64
-size 119
+oid sha256:572975888d87ee83d2134c8d598a32dbce1d7eaa58aabb9c9e6d59b82e83a9c7
+size 123
