5b2c4bbd932fc1b8a75c05d9bcd8717863eca09d
ungabunga1 <yevgenfenoga@gmail.com>
2022-11-02 23:31:43 -0400

    hallo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf421b17bd95a6f05bfd3e888406b9e0f54305301d4409c94bc7b3c862476d00
-size 126
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("welcome CIFAR 10 dataset :)");
+    }
+
+}
---
+++ src/main/resources/CIFAR10/jsonsReadMe.txt
@@ -0,0 +1,5 @@
+these jsons are the result of an unpickle function made in python, not java.
+
+these are unpickled versions of the files downloaded from the cifar read me html.
+
+even though the result of the function is included, the function itself is not.
