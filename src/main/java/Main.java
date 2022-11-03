dca0d8fc3870e8c0c6d27e228ab69dd47565ea5c
ungabunga1 <yevgenfenoga@gmail.com>
2022-11-02 20:31:28 -0400

    large file storage test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f9860a9b7866f645a947df3cff26dae163dc4320fbff609597316d4199e2076
-size 70
+oid sha256:973070cab8b3610123e0f00b34d6d28030c8b2dff38b233272761b4ae05ff1de
+size 1476678
